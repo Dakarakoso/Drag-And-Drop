365234673cdcdddf3947d07cbd6489309a5462f0
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2021-10-10 20:36:08 +0900

    adding drag and drop functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll(".solid");
 const addItemContainers = document.querySelectorAll(".add-container");
 const addItems = document.querySelectorAll(".add-item");
 // Item Lists
-const itemLists = document.querySelectorAll(".drag-item-list");
+const listsColumns = document.querySelectorAll(".drag-item-list");
 const backlogList = document.getElementById("backlog-list");
 const progressList = document.getElementById("progress-list");
 const completeList = document.getElementById("complete-list");
@@ -20,6 +20,8 @@ let onHoldListArray = [];
 let listArrays = [];
 
 // Drag Functionality
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -65,6 +67,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement("li");
   listEl.classList.add("drag-item");
   listEl.textContent = item;
+  listEl.draggable = true;
+  listEl.setAttribute("ondragstart", "drag(event)");
   columnEl.appendChild(listEl);
 }
 
@@ -97,5 +101,28 @@ function updateDOM() {
   // Run getSavedColumns only once, Update Local Storage
 }
 
+function drag(e) {
+  draggedItem = e.target;
+  console.log(draggedItem);
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function dragEnter(column) {
+  // console.log(listsColumns[column]);
+  listsColumns[column].classList.add("over");
+  currentColumn = column;
+}
+
+function drop(e) {
+  e.preventDefault();
+  listsColumns.forEach((column) => {
+    column.classList.remove("over");
+  });
+  const parent = listsColumns[currentColumn];
+  parent.appendChild(draggedItem);
+}
 // on load
 updateDOM();
